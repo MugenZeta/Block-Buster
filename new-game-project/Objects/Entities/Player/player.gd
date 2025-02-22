cb6ef6c50516c80c4c7225522d63771ac8d902d9
Harold Garner <hgarner213@gmail.com>
2025-02-22 13:45:49 -0600

    Improved Movement Code

--- new-game-project/Objects/Entities/Player/player.gd
+++ new-game-project/Objects/Entities/Player/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
+@export var MAX_SPEED = 300.0
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
+@export var ACCEL = 10.0
+@export var DECEL = 30.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		print(get_gravity())
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
@@ -15,9 +19,11 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Move_Left", "Move_Right")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCEL)
+		print(velocity.x)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, DECEL)
 
 	move_and_slide()
