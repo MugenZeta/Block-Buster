7fd7f6b7f786fd046bc68353ee2e852bbaa2b6be
Harold Garner <hgarner213@gmail.com>
2025-03-10 17:11:17 -0500

    Added Slops and beginning simple AI

--- new-game-project/Objects/Entities/Player/player.gd
+++ new-game-project/Objects/Entities/Player/player.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
 @export var MAX_SPEED = 300.0
-@export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
+@export var GRAVITY = get_gravity()
 @export var ACCEL = 10.0
-@export var DECEL = 30.0
+@export var DECEL = 0.75
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-		print(get_gravity())
+		velocity += GRAVITY * delta
+		print(GRAVITY)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
@@ -22,7 +22,6 @@ func _physics_process(delta: float) -> void:
 	
 	if direction:
 		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCEL)
-		print(velocity.x)
 	else:
 		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, DECEL)
 
